9600f3e78f6033aa79f8d0af2a2249bc953abf59
Mateusgpk <mateusgpk1@gmail.com>
2025-10-27 17:19:38 -0300

    add float

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,12 +1,12 @@
 public class Calculadora {
-    public int somar (int a, int b){
+    public float somar (float a, float b){
         return  a+b;
     }
 
-    public int subtrai (int a, int b){
+    public float subtrai (float a, float b){
         return  a-b;
     }
-    public int multi (int a, int b){
+    public float multi (float a, float b){
         return  a*b;
     }
     public float div (float a, float b){
--- src/CalculadoraGUI.java
+++ src/CalculadoraGUI.java
@@ -4,8 +4,8 @@ import java.awt.*;
 public class CalculadoraGUI extends JFrame{
     private JTextField conta;
     private StringBuilder valorAtual = new StringBuilder();
-    private int primeiroValor = 0;
-    private int segundoValor = 0;
+    private float primeiroValor = 0;
+    private float segundoValor = 0;
     private int operacao = -1; // 0=+, 1=-, 2=*, 3=/
     private Calculadora calc = new Calculadora();
     private int linha=0,dis=0;
@@ -22,7 +22,7 @@ public class CalculadoraGUI extends JFrame{
             for (int i = 9; i >0 ; i--) {
                 painel.add(CriarBotaoNumero(String.valueOf(i)));
             }
-            JPanel paineloper = new JPanel( new GridLayout(4,1, 5, 5));
+            JPanel paineloper = new JPanel( new GridLayout(5,1, 5, 5));
             paineloper.add(CriarBotaoOper("+", 0));
             paineloper.add(CriarBotaoOper("-", 1));
             paineloper.add(CriarBotaoOper("*", 2));
@@ -30,6 +30,7 @@ public class CalculadoraGUI extends JFrame{
             painel.add(CriarBotaoVoltar());
             painel.add(CriarBotaoNumero("0"));
             painel.add(CriarBotaoigual());
+            paineloper.add(CriarBotaoponto());
             add(painel, BorderLayout.CENTER);
             add(paineloper, BorderLayout.EAST);
 
@@ -53,7 +54,7 @@ public class CalculadoraGUI extends JFrame{
             btdo.addActionListener(e -> {
                 operacao = o;
                 if (valorAtual.isEmpty()) return;
-                primeiroValor = Integer.parseInt(valorAtual.toString());
+                primeiroValor = Float.parseFloat(valorAtual.toString());
                 valorAtual.setLength(0);
                 conta.setText("");
             });
@@ -64,7 +65,7 @@ public class CalculadoraGUI extends JFrame{
             btdi.setBounds(100,100,50,50);
             btdi.addActionListener(e -> {
                 if (valorAtual.isEmpty()) return;
-                segundoValor = Integer.parseInt(valorAtual.toString());
+                segundoValor = Float.parseFloat(valorAtual.toString());
                 valorAtual.setLength(0);
                 float resultado = 0;
                 try {
@@ -75,7 +76,7 @@ public class CalculadoraGUI extends JFrame{
                         case 3 -> resultado=calc.div(primeiroValor,segundoValor);
                     }
                     conta.setText(String.valueOf(resultado));
-                    primeiroValor = (int) resultado;
+                    primeiroValor = resultado;
                     valorAtual.setLength(0);
                 } catch (ArithmeticException ex) {
                     conta.setText("Erro: " + ex.getMessage());
@@ -93,6 +94,14 @@ public class CalculadoraGUI extends JFrame{
                 };
         });
             return btnv;
+        }
+        private JButton CriarBotaoponto () {
+            JButton btnp = new JButton( ".");
+            btnp.addActionListener(e -> {
+                if(!valorAtual.toString().contains("."))
+                {valorAtual.append(".");}
+            });
+            return  btnp;
         }
     }
 
